17b56762013c5eb2ee24ebdbdaecea13d0b0d7f8
Hackbright Student <user@mini-004.(none)>
2014-02-28 16:05:33 -0800

    Books read, unread, nextbook, addbook done.

--- ex5.js
+++ ex5.js
@@ -6,16 +6,61 @@ var Book = function(title, genre, author, read, readDate) {
     this.readDate = readDate || "Unknown read date";
 };
 
+
 var BookList = function(booksRead, booksNotRead, bookShelf) {
-    this.booksRead = booksRead || "You haven't read anything";
-    this.booksNotRead = booksNotRead || "Find some new books";
+    this.booksRead = numBooksRead;
+    this.booksNotRead = numBooksNotRead;
     this.bookShelf = bookShelf || "No books on shelf";
-    nextBook: function() {
-        console.log("Here is a new book:");
+    this.nextBook = nextBook;
+    this.addBook = addBook;
+    function numBooksRead(){
+        for (var i =0; i < bookShelf.length; i++) {
+        var book = bookShelf[i];
+            if (book.read == "True"){
+                booksRead.push(Book);
+            }
+        }
+        console.log("You have read " + booksRead.length + " books.");
+    }
+    function numBooksNotRead(){
+        for (var i =0; i < bookShelf.length; i++) {
+        var book = bookShelf[i];
+            if (book.read == "False"){
+                booksNotRead.push(Book);
+            }
+        }
+        console.log("You have " + booksNotRead.length + " unread books on your shelf.");
+    }
+
+    function nextBook(){
+        for (var i =0; i < bookShelf.length; i++) {
+            var book = bookShelf[i];
+            if (book.read == "False") {
+                console.log("Here is a new book: " + book.title);
+                break;
+            }
+        }
+    }
+
+    function addBook(Book){
+        bookShelf.push(Book);
+        console.log("You have added " + Book.title + " to your bookshelf.");
     }
 };
+
+
 
 var HarryPotter = new Book("Harry Potter and the Sorcerer's Stone",
  "Fantasy", "JK Rowling", "True", "2/27/14");
 
-console.log(HarryPotter);
+var MobyDick = new Book("Moby Dick", "Adventure", "Herman Melville", "True");
+
+var Twilight = new Book("Twilight", "Fantasy", "Stephanie Meyer", "False");
+
+var myList = new BookList([], [], [HarryPotter, MobyDick]);
+
+// console.log(myList.nextBook());
+console.log(myList.addBook(Twilight));
+
+console.log(myList.booksRead());
+console.log(myList.booksNotRead());
